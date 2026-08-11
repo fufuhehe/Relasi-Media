a3b79c61903394d26c30360ba3d8b641b1698e59
fufuhehe <fufuhehewkwk@gmail.com>
2026-08-11 20:47:02 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kegiatan-v1";
+const CACHE = "kegiatan-v2";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
